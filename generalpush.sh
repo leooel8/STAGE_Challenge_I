10543ba8c6faafc5306d0812cc0f3ea3bceb4db1
leooel8 <leo.ressayre@u-psud.fr>
2021-02-24 14:57:46 +0100

    Modification of generalpush

--- generalpush.sh
+++ generalpush.sh
@@ -4,14 +4,23 @@ echo "  Push service :"
 while true; do
   read -p "   Push to GitHub ? (Y/n)" yn
   case $yn in
-    [Yy]* ) echo "OK DONE";
-            git add .;
+    [Yy]* ) git add .;
             git status;
             read -p "     Type your commit message : " msg
             git commit -m "$msg";
             git push;
-            exit;;
-    [Nn]* ) exit;;
+            echo "      Git push DONE"
+            break;;
+    [Nn]* ) break;;
+    *) "Please answer yes or no.";;
+  esac
+done
+while true; do
+  read -p "   Push to Google Cloud Platform ? (Y/n)" yn
+  case $yn in
+    [Yy]* ) git push --all google;
+            break;;
+    [Nn]* ) break;;
     *) "Please answer yes or no.";;
   esac
 done
